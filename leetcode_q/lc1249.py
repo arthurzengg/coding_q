96662089e3f10711e376445203b0015d8756a3a9
arthurzengg <arthurzeng@berkeley.edu>
2024-01-04 01:40:33 -0800

    finish lc1249

---
+++ leetcode_q/lc1249.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        remove_list = []
+        valid_map = {')':'('}
+        for i in range(len(s)):
+            if s[i] in valid_map:
+                if stack:
+                    stack.pop()
+                    remove_list.pop()
+                else:
+                    remove_list.append(i)
+            else:
+                if s[i] == '(':
+                    stack.append(s[i])
+                    remove_list.append(i)
+        s = list(s)
+        for index in remove_list:
+            s[index] = ' '
+        s = ''.join(s)
+        s = s.replace(' ', '')
+        return s
+
+sol = Solution()
+print(sol.minRemoveToMakeValid("lee(t(c)o)de)"))
+# output: lee(t(c)o)de
