10f62534dd9436ebdba0cf4b87e7252903bc9c69
arthurzengg <arthurzeng@berkeley.edu>
2024-02-13 16:57:31 -0800

    update lc1010 method

--- leetcode_q/lc1010.py
+++ leetcode_q/lc1010.py
@@ -1,19 +1,20 @@
+from collections import defaultdict
+
+
 class Solution:
     def numPairsDivisibleBy60(self, time: list[int]) -> int:
-        # Dictionary to hold the count of remainders
-        remainder_count = {}
-        # Initialize the number of pairs to zero
-        num_pairs = 0
+        rem_dict = defaultdict(int)
+        res = 0
+        for num in time:
+            num %= 60
+            if num > 0:
+                res += rem_dict[60 - num]
+            else:
+                res += rem_dict[num]
 
-        for t in time:
-            # Calculate the complement that would sum with the current time to be divisible by 60
-            complement = -t % 60
-            # If the complement is in the dictionary, add the count of the complement to the number of pairs
-            num_pairs += remainder_count.get(complement, 0)
-            # Add the current remainder count to the dictionary
-            remainder_count[t % 60] = remainder_count.get(t % 60, 0) + 1
+            rem_dict[num] = rem_dict[num] + 1
 
-        return num_pairs
+        return res
 
 sol = Solution()
 print(sol.numPairsDivisibleBy60([30,20,150,100,40]))
